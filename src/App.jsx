86a5df925f7149ce5f40a55c900952f582a81ff0
Mahmodul Hasan <nicesanto92@gmail.com>
2023-10-29 19:24:10 +0600

    initial setup complete starting project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-red-500">
+      <div className="w-1/2">Left</div>
+      <div className="w-1/2">Right</div>
+    </div>
+  );
+};
+
+export default App;
