ea4605f9341696b348672a1bfb93088d8c96618b
Mahmodul Hasan <nicesanto92@gmail.com>
2023-10-30 16:20:10 +0600

    hamburger menu added navbar is now responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,14 +42,17 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const isWidth = useWidth(1000);
   const handleToggle = () => {
+    if (!isWidth) {
+      setIsOpen(false);
+    }
     setIsOpen(!isOpen);
   };
 
   const text = ["N", "I", "T", "E", "X"];
   return (
     <nav className="bg-white text-black">
-      <header className=" flex  justify-between max-w-7xl mx-auto items-center h-16 px-4 ">
-        <Link to="/" className="flex gap-3">
+      <header className=" flex  justify-between max-w-7xl mx-auto items-center py-4  px-4 relative">
+        <Link to="/" className="flex gap-1 sm:gap-3">
           {text.map((letter) => (
             <span
               key={letter}
@@ -61,28 +64,51 @@ const Navbar = () => {
         </Link>
 
         {/* Menu Icon */}
-        <ul className="md:flex gap-5 hidden">
-          {links.map((link) => (
-            <li key={link.title}>
-              <Link
-                to={link.href}
-                className="text-base uppercase cursor-pointer transition-all 
+        {!isWidth ? (
+          <ul className="flex gap-5  text-black   right-0 bg-white top-[100%] ">
+            {links.map((link) => (
+              <li key={link.title}>
+                <Link
+                  to={link.href}
+                  className="text-base uppercase cursor-pointer transition-all 
                   font-medium duration-300 hover:underline whitespace-nowrap "
-              >
-                {link.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className="md:flex gap-5 items-center hidden">
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          isOpen && (
+            <ul className="flex gap-5 flex-col text-black  absolute  right-0 bg-white top-[100%] ">
+              {links.map((link) => (
+                <li
+                  key={link.title}
+                  className="py-2
+            border-b hover:bg-zinc-900/80 hover:text-white  px-9"
+                >
+                  <Link
+                    to={link.href}
+                    className="text-base uppercase cursor-pointer transition-all 
+                  font-medium duration-300 hover:underline whitespace-nowrap "
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )
+        )}
+
+        <ul className="flex sm:gap-5 items-center">
           {loginRegisterLinks.map((link) => (
             <li key={link.title}>
               {link.title === "Login" ? (
                 <Link
                   to={link.href}
-                  className=" uppercase bg-black text-white font-medium 
-                    px-7 py-2 rounded-full 
-                    hover:bg-white hover:text-black outline
+                  className=" uppercase bg-black outline-none text-white font-medium 
+                    px-6 sm:px-7  py-2 rounded-full 
+                    hover:bg-white hover:text-black hover:outline-black
                     cursor-pointer transition-all duration-300 whitespace-nowrap "
                 >
                   {link.title}
@@ -91,7 +117,7 @@ const Navbar = () => {
                 <Link
                   to={link.href}
                   className="text-base uppercase cursor-pointer transition-all 
-               font-medium duration-300 underline"
+               font-medium duration-300 underline hidden sm:block"
                 >
                   {link.title}
                 </Link>
@@ -99,6 +125,7 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        {/* show or not show hamburger menu */}
         {isWidth ? (
           <div
             id="nav-icon1"
